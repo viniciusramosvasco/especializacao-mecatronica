d5254fa8a250515c9edb5612521f73dd183fb991
Vinícius Ramos Vasco <83826286+viniciusramosvasco@users.noreply.github.com>
2021-05-17 15:27:14 -0300

    Versão 4
    Atualização dos códigos-fonte para a 4ª versão.
    
    Essa é a versão final dos códigos-fonte utilizadas no Arduino e no PC para o teste do protótipo.

--- Sistema_Controle_Submarino/Sistema_Controle_Submarino.ino
+++ Sistema_Controle_Submarino/Sistema_Controle_Submarino.ino
@@ -2,42 +2,34 @@
 #include <EasyTransfer.h>
 EasyTransfer ET;
 
-struct rovData
+struct rovData         // As variáveis dentro dessa struct são recebidas através de transmissão serial da placa que está na superfície.
 {
-  int RSYVal;
-  int RSXVal;
-  int LSYVal;
-  boolean triangulo;
-}data;
+  int RSYVal;          // Valor do bit referente ao eixo Y do direcional analógico direito. Iniciais do inglês, "Right Stick Y Value".
+  int LSYVal;          // Valor do bit referente ao eixo y do direcional analógico esquerdo. Iniciais do inglês, "Left Stick Y Value".
+  boolean triangulo;   // Botão triângulo
+  boolean Up;          // Valor lógico referente ao botão "cima" do direcional digital.
+  boolean Down;        // Valor lógico referente ao botão "baixo" do direcional digital.
+}
+data;
 
-/*
- int LPWM_Output = 8;  //(pino 2 do IBT-2) sentido direto motor 1 --> Motor Esquerda
- int RPWM_Output = 9;  //(pino 1 do IBT-2) sentido reverso motor 1 --> Motor Esquerda
+int LPWM_Output = 8;  //(pino 2 do BTS7960B) Entrada de controle PWM do lado esquerdo da ponte H. Rotação do eixo do Thruster 1 no sentido horário.
+int RPWM_Output = 9;  //(pino 1 do BTS7960B) Entrada de controle PWM do lado direito da ponte H. Rotação do eixo do Thruster 1 no sentido anti-horário.
 
- int LPWM_Output_2 = 6;  //(pino 2 do IBT-2) sentido direto motor 2 --> Motor da Direita
- int RPWM_Output_2 = 7;  //(pino 1 do IBT-2) sentido reverso motor 2 --> Motor da Direita
+int LPWM_Output_2 = 6;  //(pino 2 do BTS7960B) Entrada de controle PWM do lado esquerdo da ponte H. Rotação do eixo do Thruster 2 no sentido horário.
+int RPWM_Output_2 = 7;  //(pino 1 do BTS7960B) Entrada de controle PWM do lado direito da ponte H. Rotação do eixo do Thruster 2 no sentido anti-horário.
 
- int LPWM_Output_3 = 3;  //(pino 2 do IBT-2) sentido direto motor 3 --> Motor Vertical
- int RPWM_Output_3 = 5;  //(pino 1 do IBT-2) sentido reverso motor 3 --> Motor Vertical
- */
+int LPWM_Output_3 = 3;  //(pino 2 do BTS7960B) Entrada de controle PWM do lado esquerdo da ponte H. Rotação do eixo do Thruster 3 no sentido horário.
+int RPWM_Output_3 = 5;  //(pino 1 do BTS7960B) Entrada de controle PWM do lado direito da ponte H. Rotação do eixo do Thruster 3 no sentido anti-horário.
+
 int RSYVal = 0;
-int RSXVal = 0;
 int LSYVal = 0;
+boolean Up = false;
+boolean Down = false;
 boolean triangulo = false;
-int rele = 26;  //Relé conectado no pino 26 do Arduino MEGA
+int rele = 22;          //Relé conectado no pino 22 do Arduino MEGA
 boolean estado_rele = LOW;
 
-int motor_speed = 12;
-int motor_speed_2 = 11;
-int motor_speed_3 = 5;
-int brightness = 0;
-int brightness_2 = 0;
-int brightness_3 = 0;
-
-int ledPin = 30;
-int led1 = 12;
-int led2 = 11;
-int led3 = 5;
+int ledPin = 13;        // Led indicador de liga/desliga
 int ledState = LOW;
 unsigned long previousMillis = 0;
 unsigned long interval = 1000;
@@ -46,26 +38,22 @@ unsigned long lastComInterval = 1500;
 
 void setup()
 {
-  Serial.begin(9600); 
+  Serial.begin(115200); 
   ET.begin(details(data), &Serial);
-  /*
-   pinMode(LPWM_Output, OUTPUT);
-   pinMode(RPWM_Output, OUTPUT);
-   pinMode(LPWM_Output_2, OUTPUT);
-   pinMode(RPWM_Output_2, OUTPUT);
-   pinMode(LPWM_Output_3, OUTPUT);
-   pinMode(RPWM_Output_3, OUTPUT);
-   */
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
+
+  pinMode(LPWM_Output, OUTPUT);
+  pinMode(RPWM_Output, OUTPUT);
+
+  pinMode(LPWM_Output_2, OUTPUT);
+  pinMode(RPWM_Output_2, OUTPUT);
+
+  pinMode(LPWM_Output_3, OUTPUT);
+  pinMode(RPWM_Output_3, OUTPUT);
+
   pinMode(ledPin, OUTPUT);
-  pinMode(motor_speed, OUTPUT);
-  pinMode(motor_speed_2, OUTPUT);
-  pinMode(motor_speed_3, OUTPUT);
   pinMode(rele, OUTPUT);
-  
   digitalWrite(rele, LOW);
+
 }
 
 void loop()
@@ -76,204 +64,84 @@ void loop()
     lastComCheck = millis();
     interval = 1000;
 
+
+    /*                               Thruster 1                                              */
+    /*****************************************************************************************/
     //Movimentação para frente
-    if(data.RSYVal < 100){
-      int motor_speed = map(data.RSYVal,0,99,99,0);
-      int motor_speed_2 = map(data.RSYVal,0,99,99,0);
-      Serial.println(motor_speed, DEC);
-      Serial.println("\t");
-      Serial.println(motor_speed_2, DEC);
-      digitalWrite(led1, LOW);    
-      digitalWrite(led2, LOW);
-      byte brightness;
-      byte brightness_2;
-      brightness = motor_speed;
-      brightness_2 = motor_speed_2;
-      analogWrite(led1, brightness);
-      analogWrite(led2, brightness_2);
-      /*
-       int forwardPWM = motor_speed;
-       analogWrite(LPWM_Output, forwardPWM);
-       analogWrite(RPWM_Output, 0);
-       
-       int forwardPWM_2 = motor_speed_2;
-       analogWrite(LPWM_Output_2, forwardPWM_2);       
-       analogWrite(RPWM_Output_2, 0);
-       */
+    if(data.LSYVal < 100){
+
+      digitalWrite(LPWM_Output, HIGH);
+      digitalWrite(RPWM_Output, LOW);              
     }
     // Parado
-    else if(data.RSYVal >= 100 && data.RSYVal < 156){
-      motor_speed = 0;
-      motor_speed_2 = 0;
-      Serial.println(motor_speed, DEC);
-      Serial.println("\t");
-      Serial.println(motor_speed_2, DEC);
-      digitalWrite(led1, LOW);
-      digitalWrite(led2, LOW);
-      //    Serial.println("Analogico na posicao Neutra!!");
-      
-      /*
-      analogWrite(LPWM_Output, 0);
-      analogWrite(RPWM_Output, 0);
-      
-      analogWrite(LPWM_Output_2, 0);
-      analogWrite(RPWM_Output_2, 0);
-      */
+    else if(data.LSYVal >= 100 && data.LSYVal < 156){
+
+      digitalWrite(LPWM_Output, LOW);
+      digitalWrite(RPWM_Output, LOW);     
     }
     //Movimentação em Ré
     else{
-      /*Serial.println("BIT: "); 
-       Serial.println(RSYVal, DEC);*/      //analógico direito
-      int motor_speed = map(data.RSYVal,156,255,0,99);
-      int motor_speed_2 = map(data.RSYVal,156,255,0,99);
-      /*Serial.println("RIGHT MOTOR REVERSE: ");*/
-      Serial.println(motor_speed, DEC);
-      Serial.println("\t");
-      Serial.println(motor_speed_2, DEC);
-      digitalWrite(led1, LOW);
-      digitalWrite(led2, LOW);
-      byte brightness;     
-      brightness = motor_speed;
-      brightness_2 = motor_speed_2;
-      analogWrite(led1, brightness);
-      analogWrite(led2, brightness_2);
-      /*
-       int reversePWM = motor_speed;
-       analogWrite(LPWM_Output, 0);
-       analogWrite(RPWM_Output, reversePWM);
-       
-       int reversePWM_2 = motor_speed_2
-       analogWrite(LPWM_Output_2, 0);
-       analogWrite(RPWM_Output_2, reversePWM_2);*/
+
+      digitalWrite(LPWM_Output, LOW);
+      digitalWrite(RPWM_Output, HIGH);       
+    }    
+    /*                               Thruster 2                                              */
+    /*****************************************************************************************/
+    //Movimentação para frente
+    if(data.RSYVal < 100){
+
+      digitalWrite(LPWM_Output_2, HIGH);
+      digitalWrite(RPWM_Output_2, LOW);              
+    }
+    // Parado
+    else if(data.RSYVal >= 100 && data.RSYVal < 156){
+
+      digitalWrite(LPWM_Output_2, LOW);
+      digitalWrite(RPWM_Output_2, LOW);      
+    }
+    //Movimentação em Ré
+    else{
+
+      digitalWrite(LPWM_Output_2, LOW);
+      digitalWrite(RPWM_Output_2, HIGH);      
+    }
+    /*                               Thruster 3                                              */
+    /*****************************************************************************************/
+    if(data.Up){       // Sentido Direto                  
+
+      digitalWrite(LPWM_Output_3, HIGH);
+      digitalWrite(RPWM_Output_3, LOW);
     }
 
+    else if(data.Down){      // Reverso      
 
-    //Movimentação para Esquerda
-    if(data.RSXVal < 100){            
-      /*Serial.println("BIT: ");
-       Serial.println(RSXVal, DEC);*/
-      int motor_speed = 0;
-      int motor_speed_2 = map(data.RSXVal,0,99,99,0);
-      Serial.println(motor_speed, DEC);
-      Serial.println("\t");
-      Serial.println(motor_speed_2, DEC);
-      digitalWrite(led1, LOW);
-      digitalWrite(led2, LOW);
-      byte brightness_2;
-      brightness_2 = motor_speed_2;
-      analogWrite(led2, brightness_2);
-      /*
-      analogWrite(LPWM_Output, 0);
-      analogWrite(RPWM_Output, 0);
-      
-      int forwardPWM_2 = motor_speed_2;
-      analogWrite(LPWM_Output_2, forwardPWM_2);
-      analogWrite(RPWM_Output_2, 0);
-       */
-    }
-    //Parado
-    else if(data.RSXVal >= 100 && data.RSXVal < 156){
-      motor_speed = 0;
-      motor_speed_2 = 0;
-      Serial.println(motor_speed, DEC);
-      Serial.println("\t");
-      Serial.println(motor_speed_2, DEC);
-      digitalWrite(led1, LOW);
-      digitalWrite(led2, LOW);
-      //    Serial.println("Analogico na posicao Neutra!!");
-
-      /*
-      analogWrite(LPWM_Output, 0);
-      analogWrite(RPWM_Output, 0);
-      
-      analogWrite(LPWM_Output_2, 0);
-      analogWrite(RPWM_Output_2, 0);
-      */
-      
-    }
-    //Movimentação para Direita
-    else{                                        
-      /*Serial.println("BIT: "); 
-       Serial.println(RSXVal, DEC);*/      //analógico direito
-      int motor_speed = map(data.RSXVal,156,255,0,99);
-      int motor_speed_2 = 0;
-      Serial.println(motor_speed, DEC);
-      Serial.println("\t");
-      Serial.println(motor_speed_2, DEC);
-      digitalWrite(led1, LOW);
-      digitalWrite(led2, LOW);
-      byte brightness;     
-      brightness = motor_speed;
-      analogWrite(led1, brightness);
-      /*
-      int forwardPWM = motor_speed;
-      analogWrite(LPWM_Output, forwardPWM);
-      analogWrite(RPWM_Output, 0);
-
-      analogWrite(LPWM_Output_2, 0);
-      analogWrite(RPWM_Output_2, 0);
-      */
+      digitalWrite(LPWM_Output_3, LOW);
+      digitalWrite(RPWM_Output_3, HIGH);      
     }
 
-    //motor3
-    if(data.LSYVal < 100){       // Sentido Direto            
-      /*Serial.println("BIT: ");
-       Serial.println(LSYVal, DEC);*/
-      int motor_speed_3 = map(data.LSYVal,0,99,99,0);
-      /*Serial.println("VERTICAL FORWARD: ");*/
-      Serial.println(motor_speed_3, DEC);
-      digitalWrite(led3, LOW);    
-      byte brightness_3;     
-      brightness_3 = motor_speed_3;
-      analogWrite(led3, brightness_3);
-      /*int forwardPWM_3 = motor_speed_3;
-       analogWrite(LPWM_Output_3, forwardPWM_3);
-       analogWrite(RPWM_Output_3, 0);*/
+    else{                    // Parado
+
+      digitalWrite(LPWM_Output_3, LOW);
+      digitalWrite(RPWM_Output_3, LOW);
     }
 
-    else if(data.LSYVal >= 100 && data.LSYVal < 156){
-      motor_speed_3 = 0;
-      Serial.println(motor_speed_3, DEC);
-      digitalWrite(led3, LOW);
-      //    Serial.println("Analogico na posicao Neutra!!");
-      /*
-      analogWrite(LPWM_Output_3, 0);
-      analogWrite(RPWM_Output_3, 0);
-      */
-    }
-
-    else{                                        // Sentido Reverso
-      /*Serial.println("BIT: "); 
-       Serial.println(RSYVal, DEC);*/      //analógico direito
-      int motor_speed_3 = map(data.LSYVal,156,255,0,99);
-      /*Serial.println("RIGHT MOTOR REVERSE: ");*/
-      Serial.println(motor_speed_3, DEC);
-      digitalWrite(led3, LOW);    
-      byte brightness_3;     
-      brightness_3 = motor_speed_3;
-      analogWrite(led3, brightness_3);
-      /*int reversePWM_3 = motor_speed_3;
-       analogWrite(LPWM_Output_3, 0);
-       analogWrite(RPWM_Output_3, reversePWM_3);*/
-    }
-    
-    /*   Acionamento de Rele para Iluminação */
-      if(data.triangulo == true){
-        Serial.println(" ************** Triangulo Pressionado! **************");
-        Serial.println(" **************     RELÉ ACIONADO! **************");
-        delay(20);
-        if(estado_rele == LOW){
-         estado_rele = HIGH;
-         digitalWrite(rele, !digitalRead(rele)); 
-        }
+    /*               Acionamento de Rele para Iluminação                                     */
+    /*****************************************************************************************/
+    if(data.triangulo == true){
+      Serial.println(" ************** Triangulo Pressionado! **************");
+      Serial.println(" **************     RELÉ ACIONADO! **************");
+      delay(20);
+      if(estado_rele == LOW){
+        estado_rele = HIGH;
+        digitalWrite(rele, !digitalRead(rele)); 
       }
-      else{
-       estado_rele = LOW; 
-      }
-   }
-        
-      
-    //*************************************************************    
+    }
+    else{
+      estado_rele = LOW; 
+    }
+  }
+
+  /*****************************************************************************************/
 
   if(millis() - lastComCheck > 1000)
   {
@@ -300,84 +168,13 @@ void loop()
 //--------------------------------------
 inline void rovSafe()
 {
-  /*digitalWrite(LPWM_Output, LOW);
-   digitalWrite(RPWM_Output, LOW);
-   digitalWrite(LPWM2_Output, LOW);
-   digitalWrite(RPWM2_Output, LOW);
-   digitalWrite(LPWM3_Output, LOW);
-   digitalWrite(RPWM3_Output, LOW);*/
+  digitalWrite(LPWM_Output, LOW);
+  digitalWrite(RPWM_Output, LOW);
+  digitalWrite(LPWM_Output_2, LOW);
+  digitalWrite(RPWM_Output_2, LOW);
+  digitalWrite(LPWM_Output_3, LOW);
+  digitalWrite(RPWM_Output_3, LOW);
 }
 //--------------------------------------
-
-/*
-inline void outputValues()
- {
- if(data.motor1 < 128) //motor1 Left
- {
- digitalWrite(motorPin1, HIGH);
- digitalWrite(motorPin2, LOW);
- }
- else if(data.motor1 > 128)
- {
- digitalWrite(motorPin1, HIGH);
- digitalWrite(motorPin2, LOW);
- }
- else if(data.motor1 == 128)
- {
- digitalWrite(motorPin1, LOW);
- digitalWrite(motorPin2, LOW);
- }
- else
- {
- digitalWrite(motorPin1, LOW);
- digitalWrite(motorPin2, LOW);
- }
- 
- if(data.motor2 < 128)  //motor2 Right
- {
- digitalWrite(motorPin3, HIGH);
- digitalWrite(motorPin4, LOW);
- }
- else if(data.motor2 > 128)
- {
- digitalWrite(motorPin3, HIGH);
- digitalWrite(motorPin4, LOW);
- }
- else if(data.motor2 == 128)
- {
- digitalWrite(motorPin3, LOW);
- digitalWrite(motorPin4, LOW);
- }
- else
- {
- digitalWrite(motorPin1, LOW);
- digitalWrite(motorPin2, LOW);
- }
- 
- 
- if(data.motor3 == 1) //motor3 verticals
- {
- digitalWrite(motorPin5, HIGH);
- digitalWrite(motorPin6, LOW);
- }
- else if(data.motor3 == 2)
- {
- digitalWrite(motorPin5, HIGH);
- digitalWrite(motorPin6, LOW);
- }
- else if(data.motor3 == 0)
- {
- digitalWrite(motorPin5, LOW);
- digitalWrite(motorPin6, LOW);
- }
- else
- {
- digitalWrite(motorPin1, LOW);
- digitalWrite(motorPin2, LOW);
- }
- } 
- */
-
-
 
 
--- Sistema_Controle_Superficie/Sistema_Controle_Superficie.ino
+++ Sistema_Controle_Superficie/Sistema_Controle_Superficie.ino
@@ -1,3 +1,4 @@
+
 #include <PS2X_lib.h>
 #include <EasyTransfer.h>
 EasyTransfer ET;
@@ -17,7 +18,8 @@ int neutral = 128;
 int verticals = 0;
 int LSYVal = 0;
 int RSYVal = 0;
-int RSXVal = 0;
+boolean Up = false;
+boolean Down = false;
 boolean enabled = false;
 boolean triangulo = false;
 
@@ -30,40 +32,42 @@ int sendInterval = 75;
 struct rovData
 {
   int RSYVal;
-  int RSXVal;
   int LSYVal;
   boolean triangulo;
-}data;
+  boolean Up;
+  boolean Down;  
+}
+data;
 
 //-------------------------------------------
 void setup()
 {
- Serial.begin(9600);
- ET.begin(details(data), &Serial);
- error = ps2x.config_gamepad(2,3,5,6, true, true);   //setup pins and settings:  GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
- 
- if(error == 0)
- {
-   Serial.println("Ready");
- }
- 
+  Serial.begin(115200);
+  ET.begin(details(data), &Serial);
+  error = ps2x.config_gamepad(2,3,5,6, true, true);   //setup pins and settings:  GamePad(clock, command, attention, data, Pressures?, Rumble?) check for error
+
+  if(error == 0)
+  {
+    Serial.println("Ready");
+  }
+
 }
 //-------------------------------------------
 void loop()
 {       
- if(error == 1) //skip loop if no controller found
-  return; 
-  
- if(type == 2)
- { //Guitar Hero Controller
- }
- else
- {   
+  if(error == 1) //skip loop if no controller found
+    return; 
+
+  if(type == 2)
+  { //Guitar Hero Controller
+  }
+  else
+  {   
     //ps2x.read_gamepad(false, vibrate);       //read controller and set large motor to spin at 'vibrate' speed
     //vibrate = ps2x.Analog(PSAB_BLUE);        //this will set the large motor vibrate speed based on
-                                               //how hard you press the blue (X) button  
+    //how hard you press the blue (X) button  
     ps2x.read_gamepad();
-    
+
     if(ps2x.ButtonPressed(PSB_START) && enabled == false)
     {
       enable = true;
@@ -76,38 +80,40 @@ void loop()
       Serial.println("disabled");
       enabled = false;
     }
-    
-                      
+
+
     LSYVal = ps2x.Analog(PSS_LY);
     RSYVal = ps2x.Analog(PSS_RY);
-    RSXVal = ps2x.Analog(PSS_RX);
     triangulo = ps2x.Button(PSB_TRIANGLE);
-    
+    Up = ps2x.Button(PSB_PAD_UP);
+    Down = ps2x.Button(PSB_PAD_DOWN);    
     motorCalc();   
- }      
+  }      
 }
 //-----------------------------------------
 inline void motorCalc()
 {
-    data.RSYVal = RSYVal;
-    data.RSXVal = RSXVal;  
-    data.LSYVal = LSYVal;
-    data.triangulo = triangulo;
-    //debug
-    /*
+  data.RSYVal = RSYVal;
+  data.LSYVal = LSYVal;
+  data.triangulo = triangulo;
+  data.Up = Up;
+  data.Down = Down;
+  //debug
+  /*
     Serial.println(data.motor1);
-    Serial.println(data.motor2);
-    Serial.println(data.motor3);
-    Serial.println("...");  
-    */  
-    
-    currentTime = millis();
-    if(currentTime - lastSend > sendInterval)
+   Serial.println(data.motor2);
+   Serial.println(data.motor3);
+   Serial.println("...");  
+   */
+
+  currentTime = millis();
+  if(currentTime - lastSend > sendInterval)
+  {
+    lastSend = currentTime;
+    if(enable == true)
     {
-      lastSend = currentTime;
-      if(enable == true)
-      {
-        ET.sendData();
-      }
+      ET.sendData();
     }
+  }
 }
+
